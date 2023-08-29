8fb695ed78eb778f499ec9ae29ef43c1ae8461b7
AntoonAdel <antonadel359@gmail.com>
2023-08-30 02:46:04 +0300

    pointers arrays strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
 
-	for (a = 0; a < n && src[a] != '\0'; a++)
+	a = 0;
+	while (a < n && src[a] != '\0')
 	{
 		dest[a] = src[a];
+		a++;
 	}
+
 	while (a < n)
 	{
 		dest[a] = '\0';
