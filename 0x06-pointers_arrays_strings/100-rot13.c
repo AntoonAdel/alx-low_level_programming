51a41d0a862a1efa21f0a4b7f7ef6a8b4e84fe70
AntoonAdel <antonadel359@gmail.com>
2023-08-30 03:03:56 +0300

    pointers arrays strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,30 @@
-a
+#include "main.h"
+
+/**
+  * rot13 - encodes a string using rot13
+  * @s: input string
+  * Return: encodes string
+  */
+
+char *rot13(char *s)
+{
+	int a;
+
+	char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char strrot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *p = s;
+
+	while (*s)
+	{
+		for (a = 0; a <= 52; a++)
+		{
+			if (*s == str[a])
+			{
+				*s = strrot[a];
+				break;
+			}
+		}
+		s++;
+	}
+	return (p);
+}
