9ad0187c06700eea2aaa82304b20d691b12dda55
AntoonAdel <antonadel359@gmail.com>
2023-09-27 01:58:38 +0300

    more singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,13 +11,10 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-
 	listint_t *new = malloc(sizeof(listint_t));
 
 	if (new == 0)
-	{
-		return (NULL);
-	}
+		return (0);
 
 	new->n = n;
 	new->next = *head;
